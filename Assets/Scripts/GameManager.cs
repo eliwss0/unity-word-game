46e42438f15cc7a514fdb82f12aaa6cf33b2ad02
Jewfro193 <eliwss0@gmail.com>
2020-07-07 20:38:14 -0500

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,6 +17,7 @@ public class GameManager : MonoBehaviour {
         {'A',1},{'B',3},{'C',3},{'D',2},{'E',1},{'F',4},{'G',2},{'H',4},{'I',1},{'J',8},{'K',5},{'L',1},{'M',3},
         {'N',1},{'O',1},{'P',3},{'Q',10},{'R',1},{'S',1},{'T',1},{'U',1},{'V',4},{'W',4},{'X',8},{'Y',4},{'Z',10}
     };
+    public string[] FoundWords;
     public Text SelectedString;
     public Text ScoreText;
     public int Score;
@@ -30,6 +31,7 @@ public class GameManager : MonoBehaviour {
         words=Dictionary.text.Split('\n');
         Score=0;
         ScoreText.text="0";
+        FoundWords;
     }
     // Update is called once per frame
     void Update() {
